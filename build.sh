659d065ba0524e6c75bc3fb034459612584bf658
Trenton <trenton@PKI-00720.local>
2026-01-27 00:43:38 -0600

    f

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 git add -A ./
 git commit -m f
 git push origin main
-ssh -i aosp.pem gitlab-runner@172.202.112.196 'cd /home/gitlab-runner/kyutai/pocket-tts-onnx-export && git pull origin main && source ../venv/bin/activate && python3 export.py'
+ssh -i ~/aosp.pem gitlab-runner@172.202.112.196 'cd /home/gitlab-runner/kyutai/pocket-tts-onnx-export && git pull origin main && source ../venv/bin/activate && python3 export.py'
