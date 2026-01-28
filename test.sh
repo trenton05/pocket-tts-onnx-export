2063371f61f2806a2c9ce1985080608036648b2e
Trenton <trenton@PKI-00720.local>
2026-01-28 11:19:35 -0600

    f

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
-adb -s WT02082500015 pull /data/data/com.is360mobile.walt/cache/output.pcm ./output.pcm
-ffmpeg -f s16le -ar 24000 -ac 1 -i ./output.pcm output.wav
+adb -s WT02082500015 pull /data/data/com.is360mobile.walt/cache/music.pcm ./music.pcm
+ffmpeg -y -f s16le -ar 24000 -ac 1 -i ./music.pcm music.wav
+
+adb -s WT02082500015 pull /data/data/com.is360mobile.walt/cache/readout.pcm ./readout.pcm
+ffmpeg -y -f s16le -ar 24000 -ac 1 -i ./readout.pcm readout.wav
 
