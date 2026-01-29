0d275389a1f79174a9b5ff07362299e3a0fca4da
Trenton <trenton@PKI-00720.local>
2026-01-29 15:13:39 -0600

    f

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ git commit -m f
 git push origin main
 ssh -i ~/aosp.pem gitlab-runner@172.202.112.196 'cd /home/gitlab-runner/kyutai/pocket-tts-onnx-export && git pull origin main && source ../venv/bin/activate && python3 export.py'
 
-scp -i ~/aosp.pem gitlab-runner@172.202.112.196:/home/gitlab-runner/kyutai/pocket-tts-onnx-export/onnx/mimi_encoder_fp16.onnx ~/Downloads/mimi_encoder.onnx
-scp -i ~/aosp.pem gitlab-runner@172.202.112.196:/home/gitlab-runner/kyutai/pocket-tts-onnx-export/onnx/mimi_decoder_fp16.onnx ~/Downloads/mimi_decoder.onnx
+scp -i ~/aosp.pem gitlab-runner@172.202.112.196:/home/gitlab-runner/kyutai/pocket-tts-onnx-export/onnx/mimi_encoder.onnx ~/Downloads/mimi_encoder.onnx
+scp -i ~/aosp.pem gitlab-runner@172.202.112.196:/home/gitlab-runner/kyutai/pocket-tts-onnx-export/onnx/mimi_decoder.onnx ~/Downloads/mimi_decoder.onnx
 
 adb -s WT02082500015 push ~/Downloads/mimi_encoder.onnx /data/mimi_encoder.onnx
 adb -s WT02082500015 push ~/Downloads/mimi_decoder.onnx /data/mimi_decoder.onnx
