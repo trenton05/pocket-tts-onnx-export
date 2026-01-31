21f07bb5a222cb9755c28461b67a8ef8f1aed29b
Trenton <trenton@PKI-00720.local>
2026-01-31 11:53:29 -0600

    f

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 git add -A ./
 git commit -m f
 git push origin main
-ssh -i ~/aosp.pem gitlab-runner@172.202.112.196 'cd /home/gitlab-runner/kyutai/pocket-tts-onnx-export && git pull origin main && source ../venv/bin/activate && python3 export.py'
+ssh -i ~/aosp.pem gitlab-runner@172.202.112.196 'cd /home/gitlab-runner/kyutai/pocket-tts-onnx-export && git checkout main && git pull origin main && source ../venv/bin/activate && python3 export.py'
 
 scp -i ~/aosp.pem gitlab-runner@172.202.112.196:/home/gitlab-runner/kyutai/pocket-tts-onnx-export/onnx/mimi_encoder.onnx ~/Downloads/mimi_encoder.onnx
 scp -i ~/aosp.pem gitlab-runner@172.202.112.196:/home/gitlab-runner/kyutai/pocket-tts-onnx-export/onnx/mimi_decoder.onnx ~/Downloads/mimi_decoder.onnx
